05c0599e744bfd4eb499af1d3d344de171a866cd
hufsicehwang <sksmsgkstlr@naver.com>
2021-01-02 23:37:25 +0900

    마커 추가

--- app/src/main/java/com/example/a2_2_teamproject/MainActivity.java
+++ app/src/main/java/com/example/a2_2_teamproject/MainActivity.java
@@ -9,10 +9,13 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.ViewGroup;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+;
+import net.daum.android.map.MapViewEventListener;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
+
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -23,36 +26,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getHashKey();
-        MapView mapView = new MapView(this);
 
+        MapView mapView = new MapView(this);
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view);
+        MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(37.33785369170434, 127.25249383940952); // 위치를 지정해 주는 MapPoint 객체 생성
+        mapView.setMapCenterPoint(mapPoint, true);
+
         mapViewContainer.addView(mapView);
 
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("My House");
+        marker.setTag(0);
+        marker.setMapPoint(mapPoint);
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
+
+
+
+
+        
+
 
 
 
     }
-    private void getHashKey(){
-        PackageInfo packageInfo = null;
-        try {
-            packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageInfo == null)
-            Log.e("KeyHash", "KeyHash:null");
-
-        for (Signature signature : packageInfo.signatures) {
-            try {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            } catch (NoSuchAlgorithmException e) {
-                Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
-            }
-        }
-    }
-
-
 }
