bb3f09737b59b3eb081d9646c0b9bcbf021e5f87
hufsicehwang <sksmsgkstlr@naver.com>
2021-01-26 16:17:36 +0900

    Animation 추가

--- app/src/main/java/com/map/a2_2_teamproject/SplashActivity.java
+++ app/src/main/java/com/map/a2_2_teamproject/SplashActivity.java
@@ -5,13 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+    TextView text1;
+    TextView text2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        text1 = findViewById(R.id.text_splash);
+        text2 = findViewById(R.id.text_Splash2);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.scale);
+        text1.startAnimation(animation);
+        text2.startAnimation(animation);
 
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -21,6 +34,6 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-        },2000);
+        },3000);
     }
 }
