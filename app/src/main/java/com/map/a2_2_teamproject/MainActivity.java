09b8d94f7ec2628a126275984c591dd4a5db2505
hufsicehwang <sksmsgkstlr@naver.com>
2021-01-26 18:33:56 +0900

    내 위치 (미구현)

--- app/src/main/java/com/map/a2_2_teamproject/MainActivity.java
+++ app/src/main/java/com/map/a2_2_teamproject/MainActivity.java
@@ -1,14 +1,25 @@
 package com.map.a2_2_teamproject;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager.widget.ViewPager;
 
 import com.google.android.material.tabs.TabLayout;
@@ -20,18 +31,27 @@ import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapPointBounds;
 import net.daum.mf.map.api.MapPolyline;
+import net.daum.mf.map.api.MapReverseGeoCoder;
 import net.daum.mf.map.api.MapView;
 
 
 
 import static net.daum.mf.map.api.MapView.setMapTilePersistentCacheEnabled;
-public class MainActivity extends AppCompatActivity implements MapView.MapViewEventListener, MapView.POIItemEventListener, MapView.OpenAPIKeyAuthenticationResultListener{
+public class MainActivity extends AppCompatActivity implements MapView.MapViewEventListener, MapView.POIItemEventListener, MapView.OpenAPIKeyAuthenticationResultListener,MapView.CurrentLocationEventListener, MapReverseGeoCoder.ReverseGeoCodingResultListener {
+    MapView mapView;
     double x;    // 위도 변수
     double y;   // 경도 변수
     int i = 0;
     Button up;
     Button down;
     Button back;
+
+    //내위치
+    private static final String LOG_TAG = "MainActivity";
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS  = {Manifest.permission.ACCESS_FINE_LOCATION};
+    private Context context;
 
 
     @Override
@@ -48,10 +68,7 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         tab.setupWithViewPager(viewPager);
 
 
-
-
-
-        final MapView mapView = new MapView(this); // MapView 객체
+        mapView = new MapView(this); // MapView 객체
         setMapTilePersistentCacheEnabled(true); // 한번 로드시 캐시에 저장
         ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view); // id 연결
 
@@ -73,8 +90,6 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         marker3.setShowAnimationType(MapPOIItem.ShowAnimationType.DropFromHeaven); // 하늘에서 날라오는 애니메이션 마커 생성 전에 선언 해야함.
 
 
-
-
         //마크1 지정!!!!!!!!!
         MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(a, b);   // 임의의 MapPoint 객체를 만듬
         marker1.setItemName("나의 위치");
@@ -83,7 +98,7 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         marker1.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
         marker1.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
         mapView.addPOIItem(marker1);
-        mapView.selectPOIItem(marker1,true); // 말풍선 항상 노출 허용
+        mapView.selectPOIItem(marker1, true); // 말풍선 항상 노출 허용
         //마크1지정!!!!!!!!!
         /* custom marker
         MapPoint mapPoint = MapPoint.mapPointWithGeoCoord(a, b);
@@ -106,11 +121,11 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         marker2.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
         marker2.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
         mapView.addPOIItem(marker2);
-        mapView.selectPOIItem(marker2,true); // 말풍선 항상 노출 허용
+        mapView.selectPOIItem(marker2, true); // 말풍선 항상 노출 허용
         //마크2지정!!!!!!!!!
 
-        final double e = (a+c) /2;
-        final double f = (b+d) /2;
+        final double e = (a + c) / 2;
+        final double f = (b + d) / 2;
 
         //마크3 지정!!!!!!!!!
         MapPoint mapPoint3 = MapPoint.mapPointWithGeoCoord(e, f);   // 임의의 MapPoint 객체를 만듬
@@ -120,7 +135,7 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         marker3.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
         marker3.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
         mapView.addPOIItem(marker3);
-        mapView.selectPOIItem(marker3,true); // 말풍선 항상 노출 허용
+        mapView.selectPOIItem(marker3, true); // 말풍선 항상 노출 허용
         //마크3지정!!!!!!!!!
 
 
@@ -131,7 +146,7 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
 
         // Polyline 좌표 지정.
         polyline.addPoint(MapPoint.mapPointWithGeoCoord(a, b));
-        polyline.addPoint(MapPoint.mapPointWithGeoCoord(c,d));
+        polyline.addPoint(MapPoint.mapPointWithGeoCoord(c, d));
         // Polyline 지도에 올리기.
         mapView.addPolyline(polyline);
 
@@ -141,10 +156,10 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding));
         //polyline!!!!!!!!!!!!
 
-        final int radi[] = {1000,2000,3000,4000,5000,10000};
+        final int radi[] = {1000, 2000, 3000, 4000, 5000, 10000};
         final int[] i = {2};
         final MapCircle circle1 = new MapCircle(
-                MapPoint.mapPointWithGeoCoord(e,f), // center
+                MapPoint.mapPointWithGeoCoord(e, f), // center
                 radi[i[0]], // radius
                 Color.argb(255, 255, 169, 77), // strokeColor
                 Color.argb(125, 173, 181, 189) // fillColor
@@ -182,12 +197,12 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {  // 확장 버튼 구현
-                i[0] = i[0] +1;
-                if(i[0]<radi.length){
+                i[0] = i[0] + 1;
+                if (i[0] < radi.length) {
                     mapView.removeCircle(circle1);
                     circle1.setRadius(radi[i[0]]);
                     final MapCircle circle1 = new MapCircle(
-                            MapPoint.mapPointWithGeoCoord(e,f), // center
+                            MapPoint.mapPointWithGeoCoord(e, f), // center
                             radi[i[0]], // radius
                             Color.argb(255, 255, 169, 77), // strokeColor
                             Color.argb(125, 173, 181, 189) // fillColor
@@ -197,25 +212,24 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
 
 
                     // 지도뷰의 중심좌표와 줌레벨을 Circle이 모두 나오도록 조정.
-                    MapPointBounds[] mapPointBoundsArray = { circle1.getBound() };
+                    MapPointBounds[] mapPointBoundsArray = {circle1.getBound()};
                     MapPointBounds mapPointBounds = new MapPointBounds(mapPointBoundsArray);
                     int padding = 200; // px
                     mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding));
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"최대 반경 입니다!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "최대 반경 입니다!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
         down.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {  // 축소 버튼 구현
-                i[0] = i[0] -1;
-                if(i[0]>=0){
+                i[0] = i[0] - 1;
+                if (i[0] >= 0) {
                     mapView.removeCircle(circle1);
                     circle1.setRadius(radi[i[0]]);
                     final MapCircle circle1 = new MapCircle(
-                            MapPoint.mapPointWithGeoCoord(e,f), // center
+                            MapPoint.mapPointWithGeoCoord(e, f), // center
                             radi[i[0]], // radius
                             Color.argb(255, 255, 169, 77), // strokeColor
                             Color.argb(125, 173, 181, 189) // fillColor
@@ -225,13 +239,12 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
 
 
                     // 지도뷰의 중심좌표와 줌레벨을 Circle이 모두 나오도록 조정.
-                    MapPointBounds[] mapPointBoundsArray = { circle1.getBound() };
+                    MapPointBounds[] mapPointBoundsArray = {circle1.getBound()};
                     MapPointBounds mapPointBounds = new MapPointBounds(mapPointBoundsArray);
                     int padding = 200; // px
                     mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding));
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"최소 반경 입니다!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "최소 반경 입니다!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -241,11 +254,215 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
         mapView.setPOIItemEventListener(this);
         onMapViewInitialized(mapView);
 
+        final Drawable mylocation_icon = getDrawable(R.drawable.ic_baseline_change_location_searching_24);
+        mapView.setCurrentLocationEventListener(this);
+        final Button mylocation = findViewById(R.id.btn_mylocation);
+        mylocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mylocation.setBackground(mylocation_icon);
+                if (!checkLocationServicesStatus()) {
+                    showDialogForLocationServiceSetting();
+                }else {
+
+                    checkRunTimePermission();
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+        mapView.setShowCurrentLocationMarker(false);
+    }
+
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint currentLocation, float accuracyInMeters) {
+        MapPoint.GeoCoordinate mapPointGeo = currentLocation.getMapPointGeoCoord();
+        Log.i(LOG_TAG, String.format("MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)", mapPointGeo.latitude, mapPointGeo.longitude, accuracyInMeters));
+    }
+
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+
+    }
+
+    @Override
+    public void onReverseGeoCoderFoundAddress(MapReverseGeoCoder mapReverseGeoCoder, String s) {
+        mapReverseGeoCoder.toString();
+        onFinishReverseGeoCoding(s);
+    }
+
+    @Override
+    public void onReverseGeoCoderFailedToFindAddress(MapReverseGeoCoder mapReverseGeoCoder) {
+        onFinishReverseGeoCoding("Fail");
+    }
+
+    private void onFinishReverseGeoCoding(String result) {
+//        Toast.makeText(LocationDemoActivity.this, "Reverse Geo-coding : " + result, Toast.LENGTH_SHORT).show();
+    }
 
 
 
 
+    /*
+     * ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드입니다.
+     */
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
 
+        if ( permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+
+            boolean check_result = true;
+
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+
+            if ( check_result ) {
+                Log.d("@@@", "start");
+                //위치 값을 가져올 수 있음
+                mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+            }
+            else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])) {
+
+                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+                    finish();
+
+
+                }else {
+
+                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+        }
+    }
+
+    void checkRunTimePermission(){
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED ) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+            // 3.  위치 값을 가져올 수 있음
+            mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+
+
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, REQUIRED_PERMISSIONS[0])) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(MainActivity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+
+
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+
+        }
+
+    }
+
+
+
+    //여기부터는 GPS 활성화를 위한 메소드들
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하실래요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+
+            case GPS_ENABLE_REQUEST_CODE:
+
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+
+                break;
+        }
+    }
+
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
     @Override
@@ -262,6 +479,7 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
     public void onMapViewZoomLevelChanged(MapView mapView, int i) {
 
     }
+
     public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint, MapPolyline polyline) {
 
     }
@@ -317,6 +535,8 @@ public class MainActivity extends AppCompatActivity implements MapView.MapViewEv
 
     @Override
     public void onDaumMapOpenAPIKeyAuthenticationResult(MapView mapView, int i, String s) {
-
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`
     }
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
